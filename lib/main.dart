6ab5b3de62430013041f5a1fe8ca2326ccb5c1de
oli <olivrg@gmail.com>
2019-04-04 22:58:39 +0100

    app title working

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,32 @@ class MyApp extends StatelessWidget {
     //A convenience widget that wraps a number of widgets that are commonly
     //required for applications implementing Material Design.
     return MaterialApp(
-        title: 'Crypto Index',
-        theme: new ThemeData(primaryColor: Colors.white), //will be used later
-        home: new Center(
-          child: new Text('crypto index app'),
-        )); //centralize the text
+      title: 'Crypto Index',
+      theme: new ThemeData(primaryColor: Colors.white), //will be used later
+      home: CryptoIndex(),
+    );
+  }
+}
+
+class CryptoIndex extends StatefulWidget {
+  @override
+  CryptoIndexState createState() => CryptoIndexState();
+}
+
+class CryptoIndexState extends State<CryptoIndex> {
+  void _pushSaved() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('CryptoIndex'),
+          actions: <Widget>[
+            new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+          ],
+        ),
+        body: new Center(
+          child: new Text('crypto app'),
+        ));
   }
 }
